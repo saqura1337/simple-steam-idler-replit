fd2d6a0ee36eb1f7d255517f9165b37879125546
saqura_paster <68153864+saqura1337@users.noreply.github.com>
2024-01-12 12:44:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ user2 = new steamUser();
 user2.logOn({"accountName": username2, "password": password2});
 user2.on('loggedOn', () => {
 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-	user2.setPersona(status);               
-	user2.gamesPlayed(games);
+	user2.setPersona(status2);               
+	user2.gamesPlayed(games2);
 });
 
 
@@ -47,8 +47,8 @@ user3 = new steamUser();
 user3.logOn({"accountName": username3, "password": password3});
 user3.on('loggedOn', () => {
 	if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
-	user3.setPersona(status);               
-	user3.gamesPlayed(games);
+	user3.setPersona(status3);               
+	user3.gamesPlayed(games3);
 });
 
 var username5 = process.env.username5;
@@ -63,8 +63,8 @@ user5 = new steamUser();
 user5.logOn({"accountName": username5, "password": password5});
 user5.on('loggedOn', () => {
 	if (user5.steamID != null) console.log(user5.steamID + ' - Successfully logged on');
-	user5.setPersona(status);               
-	user5.gamesPlayed(games);
+	user5.setPersona(status5);               
+	user5.gamesPlayed(games5);
 });
 
 var username6 = process.env.username6;
@@ -79,8 +79,8 @@ user6 = new steamUser();
 user6.logOn({"accountName": username6, "password": password6});
 user6.on('loggedOn', () => {
 	if (user6.steamID != null) console.log(user6.steamID + ' - Successfully logged on');
-	user6.setPersona(status);               
-	user6.gamesPlayed(games);
+	user6.setPersona(status6);               
+	user6.gamesPlayed(games6);
 });
 
 var username7 = process.env.username7;
@@ -95,8 +95,8 @@ user7 = new steamUser();
 user7.logOn({"accountName": username7, "password": password7});
 user7.on('loggedOn', () => {
 	if (user7.steamID != null) console.log(user7.steamID + ' - Successfully logged on');
-	user7.setPersona(status);               
-	user7.gamesPlayed(games);
+	user7.setPersona(status7);               
+	user7.gamesPlayed(games7);
 });
 
 var username8 = process.env.username8;
@@ -111,8 +111,8 @@ user8 = new steamUser();
 user8.logOn({"accountName": username8, "password": password8});
 user8.on('loggedOn', () => {
 	if (user8.steamID != null) console.log(user8.steamID + ' - Successfully logged on');
-	user8.setPersona(status);               
-	user8.gamesPlayed(games);
+	user8.setPersona(status8);               
+	user8.gamesPlayed(games8);
 });
 
 var username9 = process.env.username9;
@@ -127,8 +127,8 @@ user9 = new steamUser();
 user9.logOn({"accountName": username9, "password": password9});
 user9.on('loggedOn', () => {
 	if (user9.steamID != null) console.log(user9.steamID + ' - Successfully logged on');
-	user9.setPersona(status);               
-	user9.gamesPlayed(games);
+	user9.setPersona(status9);               
+	user9.gamesPlayed(games9);
 });
 
 var username10 = process.env.username10;
@@ -143,8 +143,8 @@ user10 = new steamUser();
 user10.logOn({"accountName": username10, "password": password10});
 user10.on('loggedOn', () => {
 	if (user10.steamID != null) console.log(user10.steamID + ' - Successfully logged on');
-	user10.setPersona(status);               
-	user10.gamesPlayed(games);
+	user10.setPersona(status10);               
+	user10.gamesPlayed(games10);
 });
 
 var username12 = process.env.username12;
@@ -159,8 +159,8 @@ user12 = new steamUser();
 user12.logOn({"accountName": username12, "password": password12});
 user12.on('loggedOn', () => {
 	if (user12.steamID != null) console.log(user12.steamID + ' - Successfully logged on');
-	user12.setPersona(status);               
-	user12.gamesPlayed(games);
+	user12.setPersona(status12);               
+	user12.gamesPlayed(games12);
 });
 
 var username13 = process.env.username13;
@@ -175,6 +175,6 @@ user13 = new steamUser();
 user13.logOn({"accountName": username12, "password": password12});
 user13.on('loggedOn', () => {
 	if (user13.steamID != null) console.log(user13.steamID + ' - Successfully logged on');
-	user13.setPersona(status);               
-	user13.gamesPlayed(games);
+	user13.setPersona(status13);               
+	user13.gamesPlayed(games13);
 });
