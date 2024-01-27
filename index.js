cc6afa90d0199e2b63f26f84b12168bd3e362668
saqura_paster <68153864+saqura1337@users.noreply.github.com>
2024-01-27 16:15:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,22 @@ var games5 = [218620];  // Enter here AppIDs of the needed games
 var status5 = 1;  // 1 - online, 7 - invisible
 
 
+user4 = new steamUser();
+user4.logOn({"accountName": username4, "password": password4});
+user4.on('loggedOn', () => {
+	if (user4.steamID != null) console.log(user4.steamID + ' - Successfully logged on');
+	user4.setPersona(status4);               
+	user4.gamesPlayed(games4);
+});
+
+var username5 = process.env.username5;
+var password5 = process.env.password5;
+// var shared_secret = process.env.shared;
+
+var games5 = [730];  // Enter here AppIDs of the needed games
+var status5 = 1;  // 1 - online, 7 - invisible
+
+
 user5 = new steamUser();
 user5.logOn({"accountName": username5, "password": password5});
 user5.on('loggedOn', () => {
@@ -71,7 +87,7 @@ var username6 = process.env.username6;
 var password6 = process.env.password6;
 // var shared_secret = process.env.shared;
 
-var games6 = [730];  // Enter here AppIDs of the needed games
+var games6 = [252490, 284160];  // Enter here AppIDs of the needed games
 var status6 = 1;  // 1 - online, 7 - invisible
 
 
@@ -87,7 +103,7 @@ var username7 = process.env.username7;
 var password7 = process.env.password7;
 // var shared_secret = process.env.shared;
 
-var games7 = [252490, 284160];  // Enter here AppIDs of the needed games
+var games7 = [730];  // Enter here AppIDs of the needed games
 var status7 = 1;  // 1 - online, 7 - invisible
 
 
@@ -97,6 +113,22 @@ user7.on('loggedOn', () => {
 	if (user7.steamID != null) console.log(user7.steamID + ' - Successfully logged on');
 	user7.setPersona(status7);               
 	user7.gamesPlayed(games7);
+});
+
+var username8 = process.env.username8;
+var password8 = process.env.password8;
+// var shared_secret = process.env.shared;
+
+var games8 = [730];  // Enter here AppIDs of the needed games
+var status8 = 1;  // 1 - online, 7 - invisible
+
+
+user8 = new steamUser();
+user8.logOn({"accountName": username8, "password": password8});
+user8.on('loggedOn', () => {
+	if (user8.steamID != null) console.log(user8.steamID + ' - Successfully logged on');
+	user8.setPersona(status8);               
+	user8.gamesPlayed(games8);
 });
 
 var username9 = process.env.username9;
@@ -111,15 +143,15 @@ user9 = new steamUser();
 user9.logOn({"accountName": username9, "password": password9});
 user9.on('loggedOn', () => {
 	if (user9.steamID != null) console.log(user9.steamID + ' - Successfully logged on');
-	user9.setPersona(status9);               
-	user9.gamesPlayed(games9);
+	user9.setPersona(status12);               
+	user9.gamesPlayed(games12);
 });
 
 var username10 = process.env.username10;
 var password10 = process.env.password10;
 // var shared_secret = process.env.shared;
 
-var games10 = [730];  // Enter here AppIDs of the needed games
+var games10 = [240];  // Enter here AppIDs of the needed games
 var status10 = 1;  // 1 - online, 7 - invisible
 
 
@@ -131,47 +163,15 @@ user10.on('loggedOn', () => {
 	user10.gamesPlayed(games10);
 });
 
-var username12 = process.env.username12;
-var password12 = process.env.password12;
-// var shared_secret = process.env.shared;
 
-var games12 = [730];  // Enter here AppIDs of the needed games
-var status12 = 1;  // 1 - online, 7 - invisible
+var games11 = [730, 570];  // Enter here AppIDs of the needed games
+var status11 = 1;  // 1 - online, 7 - invisible
 
 
-user12 = new steamUser();
-user12.logOn({"accountName": username12, "password": password12});
-user12.on('loggedOn', () => {
-	if (user12.steamID != null) console.log(user12.steamID + ' - Successfully logged on');
-	user12.setPersona(status12);               
-	user12.gamesPlayed(games12);
-});
-
-var username14 = process.env.username14;
-var password14 = process.env.password14;
-// var shared_secret = process.env.shared;
-
-var games14 = [240];  // Enter here AppIDs of the needed games
-var status14 = 1;  // 1 - online, 7 - invisible
-
-
-user14 = new steamUser();
-user14.logOn({"accountName": username14, "password": password14});
-user14.on('loggedOn', () => {
-	if (user14.steamID != null) console.log(user14.steamID + ' - Successfully logged on');
-	user14.setPersona(status14);               
-	user14.gamesPlayed(games14);
-});
-
-
-var games15 = [730, 570];  // Enter here AppIDs of the needed games
-var status15 = 1;  // 1 - online, 7 - invisible
-
-
-user15 = new steamUser();
-user15.logOn({"accountName": username15, "password": password15});
-user15.on('loggedOn', () => {
-	if (user15.steamID != null) console.log(user15.steamID + ' - Successfully logged on');
-	user15.setPersona(status15);               
-	user15.gamesPlayed(games15);
+user1 = new steamUser();
+user11.logOn({"accountName": username11, "password": password11});
+user11.on('loggedOn', () => {
+	if (user11.steamID != null) console.log(user11.steamID + ' - Successfully logged on');
+	user11.setPersona(status11);               
+	user11.gamesPlayed(games11);
 });
