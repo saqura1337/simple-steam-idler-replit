46b0ab4a6c3d357f5e5314141022c6a47d79b71c
saqura_paster <68153864+saqura1337@users.noreply.github.com>
2024-02-26 14:07:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -79,21 +79,6 @@ user5.on('loggedOn', () => {
 	user5.gamesPlayed(games5);
 });
 
-var username6 = process.env.username6;
-var password6 = process.env.password6;
-// var shared_secret = process.env.shared;
-
-var games6 = [700330, 252490];  // Enter here AppIDs of the needed games
-var status6 = 1;  // 1 - online, 7 - invisible
-
-user6 = new steamUser();
-user6.logOn({"accountName": username6, "password": password6});
-user6.on('loggedOn', () => {
-	if (user6.steamID != null) console.log(user6.steamID + ' - Successfully logged on');
-	user6.setPersona(status6);               
-	user6.gamesPlayed(games6);
-});
-
 var username7 = process.env.username7;
 var password7 = process.env.password7;
 // var shared_secret = process.env.shared;
@@ -109,20 +94,6 @@ user7.on('loggedOn', () => {
 	user7.gamesPlayed(games7);
 });
 
-var username8 = process.env.username8;
-var password8 = process.env.password8;
-// var shared_secret = process.env.sared;
-
-var games8 = [730];  // Enter here AppIDs of the needed games
-var status8 = 1;  // 1 - online, 7 - invisible
-
-user8 = new steamUser();
-user8.logOn({"accountName": username8, "password": password8});
-user8.on('loggedOn', () => {
-	if (user8.steamID != null) console.log(user8.steamID + ' - Successfully logged on');
-	user8.setPersona(status8);               
-	user8.gamesPlayed(games8);
-});
 
 var username9 = process.env.username9;
 var password9 = process.env.password9;
@@ -153,23 +124,6 @@ user11.on('loggedOn', () => {
 	user11.setPersona(status11);               
 	user11.gamesPlayed(games11);
 });
-
-
-var username12 = process.env.username12;
-var password12 = process.env.password12;
-// var shared_secret = process.env.shared;
-
-var games12 = [730, 570];  // Enter here AppIDs of the needed games
-var status12 = 1;  // 1 - online, 7 - invisible
-
-user12 = new steamUser();
-user12.logOn({"accountName": username12, "password": password12});
-user12.on('loggedOn', () => {
-	if (user12.steamID != null) console.log(user12.steamID + ' - Successfully logged on');
-	user12.setPersona(status12);               
-	user12.gamesPlayed(games12);
-});
-
 
 
 var username13 = process.env.username13;
